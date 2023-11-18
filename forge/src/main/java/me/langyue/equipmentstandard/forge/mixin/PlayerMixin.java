dee179a69dadd32180d0c95190ab959538039206
墨涤千尘 <leey319@163.com>
2023-11-19 04:01:53 +0800

    fix: 修复 forge 加载后不生效问题
    原因是漏加 forge.mixinConfigs 了，太坑了，对 forge 实在不熟
    
    # 2

--- forge/src/main/java/me/langyue/equipmentstandard/forge/mixin/PlayerMixin.java
+++ forge/src/main/java/me/langyue/equipmentstandard/forge/mixin/PlayerMixin.java
@@ -23,7 +23,7 @@ public abstract class PlayerMixin {
      * 挖掘速度
      * 原作者 @Draylar
      */
-    @ModifyVariable(method = "getDigSpeed", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/effect/MobEffectUtil;hasDigSpeed(Lnet/minecraft/world/entity/LivingEntity;)Z"), index = 2)
+    @ModifyVariable(method = "getDigSpeed", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/effect/MobEffectUtil;hasDigSpeed(Lnet/minecraft/world/entity/LivingEntity;)Z"), index = 3)
     private float getDestroySpeedMixin(float f) {
         return MixinUtils.getDestroySpeedMixin((Player) (Object) this, f);
     }
