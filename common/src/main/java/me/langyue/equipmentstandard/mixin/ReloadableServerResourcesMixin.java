5592f4a5fa29c1bb09a28cb3f1a89225dfff40d2
墨涤千尘 <leey319@163.com>
2023-11-19 04:10:05 +0800

    refactor: 删除无用的调式日志代码

--- common/src/main/java/me/langyue/equipmentstandard/mixin/ReloadableServerResourcesMixin.java
+++ common/src/main/java/me/langyue/equipmentstandard/mixin/ReloadableServerResourcesMixin.java
@@ -2,7 +2,6 @@ package me.langyue.equipmentstandard.mixin;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import me.langyue.equipmentstandard.EquipmentStandard;
 import me.langyue.equipmentstandard.MixinUtils;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
@@ -22,8 +21,6 @@ public class ReloadableServerResourcesMixin {
             at = @At(value = "INVOKE", target = "Lnet/minecraft/core/Registry;bindTags(Ljava/util/Map;)V")
     )
     private static <T> void updateRegistryTags(Registry<T> instance, Map<TagKey<T>, List<Holder<T>>> tagKeyListMap, Operation<T> original) {
-        EquipmentStandard.LOGGER.info("=======================================" + tagKeyListMap.size());
         original.call(instance, MixinUtils.bindTagsMixin(tagKeyListMap, instance.holders()));
-        EquipmentStandard.LOGGER.info("=======================================" + tagKeyListMap.size());
     }
 }
